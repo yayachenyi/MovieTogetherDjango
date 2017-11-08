72df6e4e51081aa1edc7864f30fe3d0a899b62dd
FengYichen <fengyichen@wirelessprv-10-193-141-71.near.illinois.edu>
2017-11-08 16:48:18 -0600

    css

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/
+    url(r'^mainboad/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
