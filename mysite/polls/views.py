4b8be2aecc19c1a397826e05270fa6c53ee5bdb0
Feng <feng36@fa17-cs411-36.cs.illinois.edu>
2017-11-09 11:57:40 -0600

    view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Question
+from .models import Movies
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'movie_list': Movies.objects.all()}
     return render(request, 'polls/index.html', context)
     
 def detail(request, question_id):
@@ -19,3 +19,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def mainboard(request):
+    context = {'movie_list': Movies.objects.all()}
+    return render(request, 'polls/details.html', context)
